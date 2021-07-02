a5b748ac13b2b5ab23469fe6b69b989186ca0353
ghassheee <ghassheee@gmail.com>
2021-07-02 18:13:00 +0900

    Updated

---
+++ src/location.ml
@@ -0,0 +1,71 @@
+open Misc
+open Hexa 
+open Big_int
+open IndexList
+
+(**********************************)
+(*  IMMEDIATE VALUES ON STACK     *) 
+(**********************************)
+type imm                        =
+                                | Big                       of big_int
+                                | Int                       of int
+                                | Label                     of Label.label
+                                | StorPCIndex
+                                | StorCnstrctrArgsBegin     of idx
+                                | StorCnstrctrArgsSize      of idx    (* the size depends on the cntrct id *)
+                                | InitDataSize              of idx
+                                | RntimeCntrctOffset        of idx    (* This index should be a JUMPDEST *)
+                                | RntimeMthdLabel           of idx * Syntax.mthd_head
+                                | CnstrctrCodeSize          of idx
+                                | RntimeCnstrctrOffset      of idx
+                                | RntimeCodeOffset          of idx
+                                | RntimeCodeSize
+                                | Minus                     of imm * imm
+
+let rec string_of_imm           =   function 
+  | Big b                           -> "(Big "^(string_of_big_int b)^")"
+  | Int i                           -> "(Int "^(string_of_int i)^")"
+  | Label _                         -> "Label (print label here)"
+  | StorPCIndex                     -> "StorPCIndex"
+  | StorCnstrctrArgsBegin _         -> "StorCnstrctrArgBegin (print cntrct id)"
+  | StorCnstrctrArgsSize _          -> "StorCnstrctrArgsSize (print cntrct id)"
+  | InitDataSize idx                -> "InitDataSize (print cntrct id here)"
+  | RntimeCntrctOffset _            -> "RntimeCntrctOffset (print contact id)"
+  | RntimeMthdLabel(idx,header)     -> "RntimeMthdLabel (print cntrct id, case header)"
+  | CnstrctrCodeSize idx            -> "CnstrctrCodeSize (print cntrct id)"
+  | RntimeCnstrctrOffset idx        -> "RntimeCnstrctrOffset (print cntrct id)"
+  | RntimeCodeOffset idx            -> "RntimeCodeOffset (print cntrct id)"
+  | RntimeCodeSize                  -> "RntimeCodeSize"
+  | Minus (a, b)                    -> "(- "^(string_of_imm a)^" "^(string_of_imm b)^")"
+
+let is_const_big (b:big_int)    =   function 
+  | Big b'                          -> eq_big_int b b'
+  | Int i                           -> eq_big_int (big i) b
+  | _                               -> false                      
+
+let is_const_int (i:int)        =   is_const_big (big i)
+
+
+(**********************************)
+(*         LOCATION               *) 
+(**********************************)
+type 'imm code_range            =   { code_start        : 'imm  (* byte *)
+                                    ; code_size         : 'imm  (* byte *)  }
+                                
+type 'imm stor_range            =   { stor_start        : 'imm  (* word *)
+                                    ; stor_size         : 'imm  (* word *)  }
+                                
+type calldata_range             =   { calldata_start    : int
+                                    ; calldata_size     : int               }
+
+type location                   =   Code          of imm code_range
+                                |   Stor          of imm stor_range
+                                |   Calldata      of calldata_range
+                                |   Stack         of int
+
+let string_of_location          =   function 
+    | Stor _                        -> "Storage ..."
+    | Code _                        -> "Code ..."
+    | Calldata c                    -> Printf.sprintf "Calldata offset %d, size %d" c.calldata_start c.calldata_size
+    | Stack i                       -> Printf.sprintf "Stack %d" i
+
---
+++ src/typeEnv.ml
@@ -0,0 +1,99 @@
+
+open    Printf 
+open    Misc
+open    Syntax
+open    IndexList
+open    Location
+
+module  L   = List
+module  BL  = BatList
+
+
+(****************************************************)
+(***              CONTEXT                         ***)
+(****************************************************)
+
+(* var list         := local variables in a scope       *)
+(* var list list    := the whole variables in src code  *) 
+type context                    =   { ids       :   tyVar  list list
+                                    ; evnts     :   tyEvnt list
+                                    ; retTyChkr :   (ty option->bool) option    }
+    
+let empty_ctx                   =   { ids       =   []
+                                    ; evnts     =   []
+                                    ; retTyChkr =   None    }
+
+
+let lookup_block name blk       =   getFstFilter (fun (var:tyVar)-> if var.id=name then Some var.ty else None) blk
+let lookup_id    name ctx       =   getFstFilter (lookup_block name) ctx.ids
+let lookup_evnt  name ctx       =   BL.find (fun (ev:tyEvnt)->ev.id=name) ctx.evnts
+let lookup_retTyCheck ctx       =   match ctx.retTyChkr with
+    | Some chkr                 ->  chkr
+    | None                      ->  err "undefined"
+
+let add_block ctx  blk          =   { ctx with ids          =   blk :: ctx.ids              }
+let add_evnts ctx evs           =   { ctx with evnts        =   values evs @ ctx.evnts      }
+let add_var   ctx id ty         =   match ctx.ids with
+    | t :: ts                   ->  { ctx with ids          =   ({id=id;ty=ty}::t) :: ts    }
+    | _                         ->  err"no current scope"
+let add_retTyChkr ctx tyChk     =   match ctx.retTyChkr with
+    | None                      ->  { ctx with retTyChkr    =   Some tyChk                  }
+    | Some _                    ->  err"Don't Overwrite ret-ty-checker"
+
+
+(****************************************************)
+(***              TYPED TERM                      ***)
+(****************************************************)
+    
+type argTy                      =   string * ty 
+type argArr                     =   string * ty * ty 
+
+let get_ty  (_,ty)              =   ty
+let get_tm  (x,_)               =   x
+
+let getArgTy  var               =   match var.ty with
+    | TyMap (_,_)               ->  None
+    | _                         ->  Some(var.id,var.ty)
+let getArrTy   var              =   match var.ty with
+    | TyMap(kTy,vTy)            ->  Some (var.id, kTy, vTy)
+    | _                         ->  None
+let getArgTys                   =   BL.filter_map getArgTy 
+let getArrTy_cntrct cn          =   BL.filter_map getArrTy (cn.cntrct_args)
+
+let positions_of_argLens lens   =
+    let rec loop ret used           =   function 
+        | []                        ->  L.rev ret
+        | alen::rest                ->  assert (alen>0 && alen<=32);
+                                        loop (used+32-alen::ret) (used+32) rest in 
+    loop [] 4(* signature length *) lens
+
+let argLocs_of_mthd m           =   match m.mthd_head with
+    | Default                   ->  []
+    | Method h                  ->  let sizes       = L.map calldata_size_of_arg h.mthd_args in
+                                    let positions   = positions_of_argLens sizes in
+                                    let size_pos    = L.combine positions sizes in
+                                    let locations   = L.map (fun(o,s)->Calldata{calldata_start=o;calldata_size=s}) size_pos in
+                                    let names       = L.map (fun (arg:tyVar)->arg.id) h.mthd_args in
+                                    let locEnv      = L.combine names locations in
+                                    locEnv
+
+let argTys_of_cntrct cn         =   getArgTys cn.cntrct_args
+
+let total_size_of_argTys tys    =   try     BL.sum (L.map size_of_ty tys) 
+                                    with    Invalid_argument _          -> 0
+let total_size_of_args args     =   try     BL.sum (L.map (fun arg-> size_of_ty arg.ty) args)
+                                    with    Invalid_argument _          -> 0 
+
+
+(***********************************)
+(* getInfo from contract Interface *)
+(***********************************)
+
+let find_tyMthd_in_cntrct mname tyCntrct : tyMthd option =
+    getFstFilter (fun (tyM:tyMthd) -> if tyM.id=mname then Some tyM else None) tyCntrct.tyCnMthds
+
+let find_tyMthd tyCntrcts mname = 
+    match getFstFilter (find_tyMthd_in_cntrct mname) (L.map snd tyCntrcts) with 
+    | Some ty   -> ty
+    | None      -> err ("find_tyMthd: "^mname^"Not found")
+
