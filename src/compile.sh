68f16955d6119094065cfda3165f8ba56bd58d20
ghasshee <ghasshee@gmail.com>
2022-01-24 15:32:15 +0900

    Updated

--- src/compile.sh
+++ src/compile.sh
@@ -5,15 +5,34 @@ SRC=`echo $SRC | sed -e 's/\.pen//g' `
 OUT="../js/$SRC.js"
 
 make
-
+if  [ $SRC=="" ] 
+then 
+    echo ""
+    echo "================================================"
+    echo "USAGE : specify a pen source file to comiple. " 
+    echo "================================================"
+    echo ""
+    echo "e.g. type; "
+    echo " +---------------------------------------------+"
+    echo "| $ ./compile.sh ../examples/counter.pen        |"
+    echo " +---------------------------------------------+"
+    echo ""
+    echo "then call it on geth with starting a miner; "
+    echo " +---------------------------------------------+"
+    echo "| $ ../eth/geth.sh                              |"
+    echo "| geth> miner.start(1);                         |"
+    echo "| geth> loadScript(../js/counter.js);           |"
+    echo " +---------------------------------------------+"
+    exit 1; 
+fi
 BIN=`./pen < $1`
 ABI=`./pen --abi < $1`
 
-echo "============================================="
+echo "======================================================================"
 echo "CONTRACT $SRC"
-echo "============================================="
+echo "======================================================================"
 echo $ABI
-echo "============================================="
+echo "======================================================================"
 echo $BIN 
 
 echo "var abi      = $ABI;"                                                  > $OUT
@@ -23,7 +42,7 @@ echo "var object   = {from:eth.accounts[0], data:code, gas:1000000};"       >> $
 echo "var gas      = {from:eth.accounts[0], gas:1000000};"                  >> $OUT
 echo "var instance = contract.new(object);"                                 >> $OUT
 
-echo "============================================="
+echo "======================================================================"
 echo "JS file generated at: pen/js/$SRC.js"
 echo "code"
 echo "abi" 
@@ -31,7 +50,8 @@ echo "contract"
 echo "object"
 echo "instance"
 echo "gas"
-echo "   are defined and type below code to make an instance" 
+echo "   are defined and type below code to make an instance on geth console" 
+echo "======================================================================"
 echo "var receipt  = eth.getTransactionReceipt(instance.transactionHash);"  
 echo "var address  = receipt.contractAddress;"                              
 echo "var ${SRC}   = contract.at(address);"                                 
