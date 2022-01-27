c8a15532993f2eb2f64a87f6b6b571abf48745ed
ghasshee <ghasshee@gmail.com>
2022-01-28 02:44:33 +0900

    Updated

--- src/misc.ml
+++ src/misc.ml
@@ -62,23 +62,24 @@ let rec change_fst_by_filter f  = function
     | []        ->  raise Not_found 
     | x::xs     ->  try f x::xs with Not_found -> x :: change_fst_by_filter f xs 
 
-let isNil x                     = x=[]
+let isNil x                 = x=[]
 
-let foldl                       = L.fold_left
-let foldr                       = L.fold_right
+let foldl                   = L.fold_left
+let foldr                   = L.fold_right
 
-let rec last                    = function 
-    | []                        -> err "last: empty list cannot contain the last element."
-    | [x]                       -> x
-    | x::xs                     -> last xs
+let rec last                = function 
+    | []                    -> err "last: empty list cannot contain the last element."
+    | [x]                   -> x
+    | x::xs                 -> last xs
 
-let (++)                        = L.append
-let len                         = L.length
-let tl                          = L.tl
-let zip                         = L.combine
-let unzip                       = L.split 
+let (++)                    = L.append
+let len                     = L.length
+let lookup                  = L.assoc 
+let tl                      = L.tl
+let zip                     = L.combine
+let unzip                   = L.split 
 
-let pr_ints                     = pe $ foldl (fun x xs -> pe x;xs) "" $ L.map str_of_int 
+let pr_ints                 = pe $ foldl (fun x xs -> pe x;xs) "" $ L.map str_of_int 
 
 (*****************************************)
 (**                BITS                 **)
@@ -111,25 +112,24 @@ let (%!)                    = Big_int.mod_big_int
 let print_big               = ps $ str_of_big
 
 
-
 (*****************************************)
 (**                HEX                  **)
 (*****************************************)
 
-type hex                                =   R.t
+type hex                            =   R.t
 
-let empty_hex                           =   R.empty
-let concat_hex                          =   R.concat2
-let str_of_hex ?prefix:(prefix="")h     =   R.to_string(concat_hex(R.of_string prefix)h)
+let empty_hex                       =   R.empty
+let concat_hex                      =   R.concat2
+let str_of_hex ?prefix:(prefix="")h =   R.to_string(concat_hex(R.of_string prefix)h)
 
-let hex_of_big b len                    =   let s           =   B.to_string_in_hexa b         in
-                                            let char_len    =   2 * len                       in (* 1 char = 1/2 byte *) 
-                                            if S.length s>char_len then err "hex_of_big: too big" ; 
-                                            let prefix      =   S.make(char_len-S.length s)'0' in
-                                            concat_hex(R.of_string prefix)(R.of_string s)
+let hex_of_big b len                =   let s           =   B.to_string_in_hexa b         in
+                                        let char_len    =   2 * len                       in (* 1 char = 1/2 byte *) 
+                                        if S.length s>char_len then err "hex_of_big: too big" ; 
+                                        let prefix      =   S.make(char_len-S.length s)'0' in
+                                        concat_hex(R.of_string prefix)(R.of_string s)
 
-let pr_hex ?prefix:(prefix="")h         =   pf "%s\n"(str_of_hex ~prefix h)
-let hex_of_str s                        =   R.of_string s  
+let pr_hex ?prefix:(prefix="")h     =   pf "%s\n"(str_of_hex ~prefix h)
+let hex_of_str s                    =   R.of_string s  
 
 
 
@@ -148,7 +148,6 @@ let idx_sort         l  =   L.sort (fun a b -> compare (fst a)(fst b)) l
 let map              f  =   L.map  (fun(i,x)-> (i,f x)) 
 let imap             f  =   L.map  (fun(i,x)-> (i,f i)) 
 let filter_map       f  =   BL.filter_map (fun(i,x)->BO.map(fun y->i,y)(f x))
-let lookup              =   L.assoc 
 let pr_imap          f  =   L.iter (fun i -> pf "%d ↦ %d, "i(f i))
 let insert i a l        =   (i,a)::l       (* shall I sort it?  Maybe later at once. *)
 let lookup_idx f l      =   let i,_ = L.find (f $ snd) l in i 
