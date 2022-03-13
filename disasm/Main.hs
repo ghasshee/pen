ed25439130873db5548637127369be8a2f93c9b7
ghasshee <ghasshee@gmail.com>
2022-03-13 22:54:26 +0900

    Updated

--- disasm/Main.hs
+++ disasm/Main.hs
@@ -7,11 +7,15 @@ import VM
 
 
 main = do 
+    -- | Lexer 
     words <- bytes 
+    -- | DisAsm 
     let prog = lineNo $ disAsm words 
     prAsm prog
+    -- | Cut Asm 
     let progs = cuts $ extract prog 
     mapM print $ progs 
+    -- | Decompile
     let tree = fparse progs
     print tree
 
