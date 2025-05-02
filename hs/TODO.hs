b06fc77d25ed56b5efa69eb1f5a15e2d5fe5e666
ghasshee <ghasshee@gmail.com>
2025-05-03 01:08:12 +0900

    Updated

--- hs/TODO.hs
+++ hs/TODO.hs
@@ -23,3 +23,20 @@
 --
 --
 -- #TODO Parser.y  -- removal of LET Sugar Syntax 
+--
+--
+--
+--
+--
+-- The semiring automaton graph : it's the program
+-- duplicate them into two worlds, A and B. 
+-- we start from the world A and if we satisfied required condition, 
+-- we move to the other world B. 
+--
+-- If we have two required conditions, 
+-- we go from A to B, then we go from B1 to B2. 
+-- if two conditions are ordered, then we cannot go from A1 through A2 to B2. 
+-- That is implemented by Kripke Possible World using Temporal Logical statements. 
+--
+--
+--
