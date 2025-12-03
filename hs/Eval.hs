b576e9febaa1c40a4842480a80bfb161fffab520
ghasshee <ghasshee@gmail.com>
2025-12-03 15:12:27 +0900

    #CLEANing up Typing.hs

--- hs/Eval.hs
+++ hs/Eval.hs
@@ -25,64 +25,12 @@ eval ctx stx tr = tr
 
 
 
-processCN :: Ctx -> Ctx -> UVar -> Constraint -> CONTRACT -> (CONTRACT, Ctx, Ctx, UVar, Constraint) 
-processCN ctx stx q constr (CN id tops) = (CN id tops', ctx', stx', q', constr') where 
-    (tops', ctx', stx',  q', constr')   = processTOPs ctx stx q constr tops
+processCN :: CONTRACT -> CONTRACT 
+processCN cn = (fst3 . reconCN [] [] 0) cn    
 
-processTOPs :: Ctx -> Ctx -> UVar -> Constraint -> [TOP] -> ([TOP], Ctx, Ctx, UVar, Constraint) 
-processTOPs ctx stx q constr tops = case tops of 
-    []                                  -> ([], ctx, stx, q, constr) 
-    SV id ty                      : ts  -> (SV id ty'' : ts', ctx', stx'', q', constr') where 
-        ty''                            = apply_constr sol ty' 
-        sol                             = unify constr'
-        ty'                             = TyID (var q) 
-        stx'                            = addBind stx id (BindTmVAR ty')
-        (ts', ctx',stx'', q', constr')  = processTOPs ctx stx' (q+1) constr ts
-    MT id ty ps body : ts  -> (m' : ts', ctx'',stx', q'', constr'') where
-        tyR                             = TyID (var q) 
-        tyM                             = paramret2ty ps tyR
-        (body', tyR',ctx',q',constr')   = processBody ctx stx q  constr body
-        m'                              = MT id tyR' ps body'  
-        (ts', ctx'', stx', q'',constr'')= processTOPs ctx' stx q' constr' ts
+--typingTest :: CONTRACT -> (CONTRACT, Ctx, Ctx, UVar, Constraint) 
+-- typingTest cn = processCN [] [] 0 [] cn  
 
-processBody :: Ctx -> Ctx -> UVar -> Constraint -> BODY -> (BODY, Ty, Ctx, UVar, Constraint) 
-processBody ctx stx q constr body = 
-    let (body', tyT, q', constr')  = reconBODY ctx stx q body in 
-    let constr''            = constr ++ constr' in 
-    let sol                 = unify constr'' in 
-    let tyT'                = double (apply_constr sol) tyT in 
-    (body', tyT', ctx, q', constr'') 
-{--
-processTOPs :: Ctx -> Ctx -> UVar -> Constraint -> [TOP] -> ([TOP], Ctx, Ctx, UVar, Constraint) 
-processTOPs ctx stx q constr tops = case tops of 
-    []                                  -> ([], ctx, stx, q, constr) 
-    SV id ty                      : ts  -> (SV id ty' : ts', ctx', stx'', q', constr') where 
-        ty'                             = TyID (var q) 
-        stx'                            = addBind stx id (BindTmVAR ty')
-        (ts', ctx',stx'', q', constr')  = processTOPs ctx stx' (q+1) constr ts
-    MT id ty ps body : ts  -> (m' : ts', ctx'',stx', q'', constr'') where
-        tyR                             = TyID (var q) 
-        tyM                             = paramret2ty ps tyR
-        (body', tyR',ctx',q',constr')   = processBody ctx stx q  constr body
-        m'                              = MT id tyR' ps body'  
-        (ts', ctx'', stx', q'',constr'')= processTOPs ctx' stx q' constr' ts
-        
-
-processBody :: Ctx -> Ctx -> UVar -> Constraint -> BODY -> (BODY, Ty, Ctx, UVar, Constraint) 
-processBody ctx stx q constr body = 
-    let (body', tyT, q', constr')  = reconBODY ctx stx q body in 
-    let constr''            = constr ++ constr' in 
-    let sol                 = unify constr'' in 
-    let tyT'                = double (apply_constr sol) tyT in 
-    (body', tyT', ctx, q', constr'') 
---}
-
-
-fst5 (a,_,_,_,_) = a 
-
-typingTest :: CONTRACT -> (CONTRACT, Ctx, Ctx, UVar, Constraint) 
-typingTest cn = processCN [] [] 0 [] cn  
-
+typingTest2 :: CONTRACT -> (CONTRACT, UVar, Constraint) 
 typingTest2 cn = reconCN [] [] 0  cn 
 typing = fst3 . typingTest2
---typing = fst5 . typingTest
